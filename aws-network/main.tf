7fc82fa4e93e060a4c32167e9756b1a62bdba763
mesatya <satyajitrout.95@gmail.com>
2022-01-17 16:11:09 +0530

    added aws-network files

---
+++ aws-network/main.tf
@@ -0,0 +1,103 @@
+
+# Internet VPC
+resource "aws_vpc" "aws-vpc" {
+  cidr_block           = var.VPC_CIDR
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink   = "false"
+  tags = {
+    Name = "aws-vpc"
+  }
+}
+
+
+# Subnets
+
+resource "aws_subnet" "aws-vpc-private-subnet-1" {
+  vpc_id                  = aws_vpc.aws-vpc.id
+  cidr_block              = "172.0.1.0/24"
+  map_public_ip_on_launch = "false"
+  availability_zone       = "${var.AWS_REGION}a"
+
+  tags = {
+    Name = "aws-vpc-private-subnet-1"
+  }
+}
+
+
+
+resource "aws_security_group" "allow-ssh-icmp-aws" {
+  vpc_id      = aws_vpc.aws-vpc.id
+  name        = "allow-ssh-icmp"
+  description = "security group that allows ssh and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow-ssh-icmp-aws"
+  }
+}
+
+resource "aws_key_pair" "mykeypairaws" {
+  key_name   = "mykeyaws"
+  public_key = file("${var.PATH_TO_PUBLIC_KEY}")
+}
+
+resource "aws_instance" "aws_private_instance" {
+  ami            = lookup(var.AMIS, var.AWS_REGION)
+  instance_type  = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.aws-vpc-private-subnet-1.id
+
+  # the security group
+  vpc_security_group_ids = ["${aws_security_group.allow-ssh-icmp-aws.id}"]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypairaws.key_name
+  
+  tags = {
+   Name = "aws-private-instance"
+  }
+}
+
+resource "aws_customer_gateway" "customer-gateway" {
+  bgp_asn = 65000
+  ip_address = "75.101.176.126"
+  type       = "ipsec.1"
+
+  tags = {
+    Name = "main-customer-gateway"
+  }
+}
+
+resource "aws_vpn_gateway" "vpn-vpg" {
+  vpc_id = aws_vpc.aws-vpc.id
+
+  tags = {
+    Name = "vpn-vpg"
+  }
+}
+
+resource "aws_vpn_connection" "aws-vpn" {
+  vpn_gateway_id      = aws_vpn_gateway.vpn-vpg.id
+  customer_gateway_id = aws_customer_gateway.customer-gateway.id
+  type                = "ipsec.1"
+  static_routes_only  = true
+  local_ipv4_network_cidr = "10.0.0.0/16"
+  remote_ipv4_network_cidr = "172.0.0.0/16"
+  tags = {
+    Name = "AWS_VPN"
+  }
+}
